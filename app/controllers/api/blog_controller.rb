514858dd1898ed7844c4457ea7e587d51a492e0a
Guillaume Barillot <gbarillot@gmail.com>
2012-10-24 14:43:35 +0200

    cleanup API namespace controllers

--- app/controllers/api/blog_controller.rb
+++ app/controllers/api/blog_controller.rb
@@ -3,14 +3,14 @@ class Api::BlogController < ApplicationController
   before_filter :load_conf
 
   # GET /api/blog/feed
-  # Ouput RSS feed                                          XML
+  # Output RSS feed                                          XML
   # -----------------------------------------------------------
   def feed
 
     @posts = Post.order('created_at DESC').all
 
     if @posts.empty?
-      render :text => ''
+      render :nothing
     else
       headers['Content-Type'] = 'application/xml'
       respond_to do |format|
