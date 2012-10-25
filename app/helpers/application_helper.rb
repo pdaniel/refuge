1b44003a0479b27eae5fbef378c97c4ffe687f6a
Guillaume Barillot <gbarillot@gmail.com>
2012-10-26 00:38:58 +0200

    move thumbnail_tag generator to a dedicated helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -64,37 +64,6 @@ module ApplicationHelper
     end
   end
 
-  # Cache Dragonfly thumbnail generation and produce friendly SEO URLs
-  def thumbnail_tag(image, size, options={})
-
-    image ? this_image = image : this_image = $conf.default_avatar
-
-    sub_dir    = 'images'
-    root_dir   = File.join(Rails.root, 'public', sub_dir)
-    resource   = File.join(root_dir, size, this_image.name)
-
-    unless File.exists?(resource)
-      this_image.thumb(size).to_file(resource)
-    end 
-
-    attrs = ''
-    options.each do |key, value| 
-      if key == :data        
-        value.each do |key, value|
-          attrs << "data-#{key}='#{value}' "
-        end
-      else
-        attrs << "#{key.to_s}='#{value}' "
-      end
-    end
-
-    attrs << "alt='#{this_image.name}'"         if options[:alt].blank? 
-    width  = "width='#{size.split('x')[0]}px'"  unless size.split('x')[0].blank?  
-    height = "height='#{size.split('x')[1]}px'" unless size.split('x')[1].blank? 
-
-    "<img src='/#{sub_dir}/#{size}/#{this_image.name}' #{width} #{height} #{attrs} />".html_safe
-  end
-
   # Format gauge width for surveys
   def gauge(percentage)
     (((percentage.to_f/100.to_f)*490.to_f)+30.to_f).to_i
