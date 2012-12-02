99ee3419bd005e1f8ae5f631838b1aac50ee476c
Guillaume Barillot <gbarillot@gmail.com>
2012-12-02 21:35:15 +0100

    do not access database to get gardien credentials

--- lib/gardien.rb
+++ lib/gardien.rb
@@ -2,8 +2,8 @@ module Gardien
   class Base < ActiveResource::Base
     self.site = "https://gardien.herokuapp.com/api"
     self.timeout = 15
-    self.user = $conf.gardien_login
-    self.password = $conf.gardien_password
+    self.user = 'refuge'
+    self.password = 'dsj8938hfiuehjf9sdu0f9jk3jnqro'
     
     self.format = :json
   end
