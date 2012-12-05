78173df7ff89fceab8d2a56194ef473ef33e763c
Guillaume Barillot <gbarillot@gmail.com>
2012-12-05 09:46:16 +0100

    handle gardiens downtime

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,21 +1,25 @@
 class Location < ActiveRecord::Base
-  has_many :members, :dependent=>:destroy
-  has_many :surveys, :dependent=>:destroy
-  has_many :articles, :dependent=>:destroy
+  has_many :members,  :dependent => :destroy
+  has_many :surveys,  :dependent => :destroy
+  has_many :articles, :dependent => :destroy
     
   def self.occupation_rates
 
     @locations = []
-    Gardien::Location.all.each do |location|
-      this_location = Location.find_by_name(location.name)
 
-      rate = (location.members.length.to_f / this_location.max_occupation.to_f)*100.to_f.round(2)
+    begin
+      Gardien::Location.all.each do |location|
+        this_location = Location.find_by_refuge_id(location.id)
 
-      @locations.push({
-        :id => location.id,
-        :name => location.name,
-        :occupation => rate
-      })
+        rate = (location.members.length.to_f / this_location.max_occupation.to_f)*100.to_f.round(2)
+
+        @locations.push({:id => this_location.id, :name => this_location.name, :occupation => rate})
+      end
+    rescue
+      # Always return a populated/valid hash, even if Gardien is down 
+      self.all.each do |location|
+        @locations.push({:id => location.id, :name => location.name, :occupation => 0})
+      end
     end
 
     return @locations
